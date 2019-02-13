697e13d921ff390df2069f0072a287766ccfc4b0
Robert Pedersen <rob@worldcrimeleague.org>
2019-02-13 22:08:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+parkway =[]
+
+def line(parkway)
+  if parkway.empty?
+    puts "The line is currently empty."
+  else
+    currently = "The line is currently:"
+    parkway.each.with_index(1) do |person, i|
+      currently << " #{i}. #{person}"
+    end
+    puts currently
+  end
+end
+
+def take_a_number(parkway, person)
+  parkway << person
+  puts "Welcome, #{person}. You are number #{parkway.length} in line."
+end
+
+def now_serving(parkway)
+  if parkway.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{parkway.first}."
+    parkway.shift
+  end
+end
